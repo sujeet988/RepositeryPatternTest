3a42046332ccc443d2bc8476855ae9ab3bab21e1
sujeet kumar <sujeetkumar2010@live.com>
2020-05-10 13:41:04 +0530

    Add project files.

---
+++ WebApplication20/Repository/EmployeeRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication20.DAL;
+using System.Data.Entity;
+
+
+namespace WebApplication20.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+
+        private readonly EmployeeDBEntities _context;
+        public EmployeeRepository()
+        {
+            _context = new EmployeeDBEntities();
+        }
+
+        public EmployeeRepository(EmployeeDBEntities context)
+        {
+            _context = context;
+        }
+
+
+
+        public void Delete(int EmployeeID)
+        {
+            Employee employee = _context.Employees.Find(EmployeeID);
+            _context.Employees.Remove(employee);
+
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetById(int EmployeeID)
+        {
+            return _context.Employees.Find(EmployeeID);
+        }
+
+
+
+        public void Insert(Employee employee)
+        {
+            _context.Employees.Add(employee);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+
+        }
+
+        public void Update(Employee employee)
+        {
+            _context.Entry(employee).State = EntityState.Modified;
+        }
+
+
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+
+    }
+}
---
+++ WebApplication20/Repository/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication20.DAL;
+
+namespace WebApplication20.Repository
+{
+  public  interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee GetById(int EmployeeID);
+
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+
+
+    }
+}
